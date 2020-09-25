b21005690ee1f4e8690be89096465088a2c85ee4
Gogo <gogo81745.moe@gmail.com>
2020-09-25 11:27:49 +0800

    feature: add ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
   router,
