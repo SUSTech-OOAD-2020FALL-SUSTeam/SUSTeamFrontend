bca35044ad04e2db2ca48def87e93dee51d23999
Gogo <gogo81745.moe@gmail.com>
2020-09-26 21:35:48 +0800

    feature: add PageFooter

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,5 +1,17 @@
 <template>
-  <footer>PageFooter</footer>
+  <footer>
+    <div class="copyright">
+      © 2020, SUSteam, Org. 版权所有
+    </div>
+    <div class="legal">
+      <router-link to="/termsofservice">
+        服务条款
+      </router-link>
+      <router-link to="/privacypolicy">
+        隐私政策
+      </router-link>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -16,6 +28,32 @@ export default Vue.extend({
 footer {
   color: #e7e7e7;
   background-color: #202020;
+  padding: 1em 1.5em;
+}
+
+.copyright {
+  font-size: 0.75rem;
+  padding: 1.5em 0;
+}
+
+.legal {
+  padding: 1.5em 0;
+  font-size: 0.75rem;
+  a {
+    margin-right: 1.5em;
+    color: white;
+    transition: color .2s;
+
+    &:hover {
+      color: #0078f2
+    }
+  }
+}
+
+@media (min-width: 1024px) {
+  footer {
+    padding: 1.25em 5em 2.5em;
+  }
 }
 
 </style>
