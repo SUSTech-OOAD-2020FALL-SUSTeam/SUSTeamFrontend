1728b7b77167eeafa2e360629eb3aca129244009
Gogo <gogo81745.moe@gmail.com>
2020-11-24 17:31:21 +0800

    fix: yarn lock

--- src/background.ts
+++ src/background.ts
@@ -12,8 +12,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
