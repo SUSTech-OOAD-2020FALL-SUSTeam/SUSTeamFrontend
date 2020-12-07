0e3832416b68ed54c07e8cff22a83997b37078ac
macromogic <tonyh0411@gmail.com>
2020-12-07 21:06:06 +0800

    fix: handle purchase failure

--- src/api/Order.ts
+++ src/api/Order.ts
@@ -1,5 +1,7 @@
 import request from '@/utils/Request'
 
-export async function createOrder (gameId: number, price: number): Promise<void> {
-  return await request.post('/order', { gameId, price })
+export async function createOrder (gameId: number, price: number): Promise<boolean> {
+  const data: any = await request.post('/order', { gameId, price })
+  const status = data.status
+  return !(status === 'FAIL')
 }
